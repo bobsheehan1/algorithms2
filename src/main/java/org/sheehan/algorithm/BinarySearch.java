3050adf8a4f2e1714cdcf8513367cd10bc56b657
bsheehan <bsheehan@baymoon.com>
2014-05-19 17:38:58 -0700

    update

---
+++ src/main/java/org/sheehan/algorithm/BinarySearch.java
@@ -0,0 +1,28 @@
+package org.sheehan.algorithm;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bsheehan
+ * Date: 5/19/14
+ * Time: 12:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BinarySearch {
+
+    static int binarySearch(Integer array[], int key) {
+
+        int l = 0;
+        int u = array.length;
+
+        while (l <= u) {
+            int m = l + (u-l)/2;
+            if (array[m] > key) {
+                u = m-1;
+            }  else if (array[m] < key) {
+                l = m+1;
+            }  else
+                return m;
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/org/sheehan/algorithm/BinarySearchTest.java
@@ -0,0 +1,46 @@
+package org.sheehan.algorithm;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bsheehan
+ * Date: 5/19/14
+ * Time: 1:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BinarySearchTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testBinarySearch() throws Exception {
+        final int limit = 20;
+        Random random = new Random();
+        List<Integer> dataList = new ArrayList<Integer>();
+        for (int i = 0; i < limit; i++) {
+            dataList.add(random.nextInt());
+        }
+        Collections.sort(dataList);
+        Integer[] array = dataList.toArray(new Integer[0]);
+
+        BinarySearch bs = new BinarySearch();
+
+        for (int i = 0; i < limit; i++) {
+            System.out.print(dataList.get(i) + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < limit; i++)
+            Assert.assertEquals(i,BinarySearch.binarySearch(array, array[i]));
+    }
+}
